552e411bf27feb1cd673d998741605a8914ea21c
david <hyt152004@gmail.com>
2024-08-19 14:41:55 -0700

    exercises 1.1-1.2

---
+++ Part1/courseInfo/src/App.jsx
@@ -0,0 +1,54 @@
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Content = (props) => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  return (
+    <div>
+      <Part part={part1} exercises={exercises1}/>
+      <Part part={part2} exercises={exercises2}/>
+      <Part part={part3} exercises={exercises3}/>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.e1 + props.e2 + props.e3}</p>
+  )
+}
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const exercises1 = 10
+  const exercises2 = 7
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content/>
+      <Total e1={exercises1} e2={exercises2} e3={exercises3}/>
+    </div>
+  )
+}
+
+export default App
